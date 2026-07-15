ab1abc602bf5862d03c1f8f06e471a3cf8f675e4
Sudhakar Muktha <sudhakar.muktha@gmail.com>
2026-07-15 09:56:39 +0530

    Update argocd-n.sh

--- argocd-n.sh
+++ argocd-n.sh
@@ -13,7 +13,6 @@ kubectl get pods -A
 
 sleep 10
 kubectl create namespace argocd
-kubectl create namespace argocd
 kubectl apply -n argocd --server-side --force-conflicts -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml
 kubectl get pods -n argocd
 kubectl patch svc argocd-server -n argocd -p '{"spec": {"type": "NodePort"}}'
